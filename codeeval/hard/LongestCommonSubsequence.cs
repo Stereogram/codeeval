2de4975ffdfea00415f4dbaab3da6493ceba9098
Rian Neufelt <RNeufelt@live.ca>
2014-07-23 21:08:41 -0700

    stuff

---
+++ codeeval/hard/LongestCommonSubsequence.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace codeeval.hard
+{
+    public class LongestCommonSubsequence
+    {
+        //private static void Main(string[] args)
+        //{
+        //    foreach (var result in File.ReadAllLines(args[0]).Select(x => x.Split(';')))
+        //    {
+        //        var list0 = result[0].ToList().AllCombinations().Select(x => new string(x.ToArray()));
+        //        var list1 = result[1].ToList().AllCombinations().Select(x => new string(x.ToArray()));
+        //        foreach (var word in list0.Where(list1.Contains))
+        //        {
+        //            Console.WriteLine(word.ToArray());
+        //            break;
+        //        }
+        //    }
+        //}
+    }
+
+    public static class Ext
+    {
+        public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> source, int n)
+        {
+            if (n == 0)
+                yield return Enumerable.Empty<T>();
+
+            int count = 1;
+            foreach (T item in source)
+            {
+                foreach (var innerSequence in source.Skip(count).Combinations(n - 1))
+                {
+                    yield return new T[] {item}.Concat(innerSequence);
+                }
+                count++;
+            }
+        }
+
+        public static IEnumerable<IEnumerable<T>> AllCombinations<T>(this IList<T> source)
+        {
+            IEnumerable<IEnumerable<T>> output = Enumerable.Empty<IEnumerable<T>>();
+            for (int i = source.Count; i >= 0; i--)
+            {
+                output = output.Concat(source.Combinations(i));
+            }
+            return output;
+        }
+    }
+
+}
---
+++ codeeval/moderate/SumOfIntegers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace codeeval.moderate
+{
+    public class SumOfIntegers
+    {
+        //static void Main(string[] args)
+        //{
+        //    foreach (int max in from result in File.ReadAllLines(args[0]).Select(x => x.Split(','))
+        //        select result.Select(int.Parse).ToArray()
+        //        into arr
+        //        select (from i in Enumerable.Range(0, arr.Length)
+        //            from j in Enumerable.Range(0, arr.Length - i + 1)
+        //            where j >= 2
+        //            select arr.SubArray(i, j))
+        //        into query
+        //        select query.Select(nums => nums.Sum()).Concat(new[] {0}).Max())
+        //    {
+        //        Console.WriteLine(max);
+        //    }
+        //}
+    }
+
+    public static class Ext
+    {
+        public static T[] SubArray<T>(this T[] data, int index, int length)
+        {
+            T[] result = new T[length];
+            Array.Copy(data, index, result, 0, length);
+            return result;
+        }
+    }
+
+}
